faec49491942c46f2f241d47c9fef3aff7ce2b6a
Aishwarya N <aish623n@gmail.com>
2022-06-24 13:33:59 +0530

    feat:add header component

---
+++ pages/collections/[collectionId].js
@@ -0,0 +1,10 @@
+import React from "react";
+import {useRouter} from 'next/router';
+
+const Collection = () => {
+    const router = useRouter();
+
+    return <div></div>
+}
+
+export default Collection;
